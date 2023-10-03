3b6b86e305ae05da9754b65b50de80d3e7515548
Timothy Uong <127487224+17tuong@users.noreply.github.com>
2023-10-03 13:21:04 +0100

    Update symbol.py

--- symbol.py
+++ symbol.py
@@ -4,44 +4,43 @@ from PyQt5.QtGui import *
 from PyQt5.QtCore import *
 from PyQt5.QtWidgets import *
 import yfinance as yf
+import pandas as pd
 
 symbolwin = uic.loadUiType(r"C:\Users\chomp\Documents\VSC\Coursework\UI\symbol.ui")[0]
 
-class WindowClass(QtWidgets.QMainWindow, symbolwin):
-    def __init__(self, stock_data, parent = None):
+def get_commodity_data():
+    commodity_symbols = ['GLD', 'SLV', 'USO', 'UNG', 'CORN', 'WEAT', 'SOYB', 'JO', 'NIB', 'BAL', 'SGG']
+    combined_data = None
+    for symbol in commodity_symbols:
+        df = yf.Ticker(symbol)
+        commodity_data = df.history(period='1d')
+        commodity_data.insert(0, 'Symbol', df.info.get('symbol', 'N/A'))
+        commodity_data.insert(1, 'Name', df.info.get('longName', 'N/A'))
+        if combined_data is None:
+            combined_data = commodity_data
+        else:
+            combined_data = pd.concat([combined_data, commodity_data])
+    return combined_data
+
+class Ui_SymbolWindow(QtWidgets.QMainWindow, symbolwin):
+    def __init__(self, parent = None):
         QtWidgets.QMainWindow.__init__(self, parent)
         self.setupUi(self); 
-        self.active_data(stock_data)
-        
-    def active_data(self, stock_data):
-        symboltable = self.symboltable
-        rows, cols = len(stock_data), len(stock_data[0])
-        symboltable.setRowCount(rows)
-        symboltable.setColumnCount(cols)
-        headers = ['Symbol', 'Company Name', 'Last Price', 'Volume', 'Market Cap']
-        self.symboltable.setHorizontalHeaderLabels(headers)
-        for i, row_data in enumerate(stock_data):
-            for j, cell_data in enumerate(row_data):
-                item = QTableWidgetItem(str(cell_data))
-                symboltable.setItem(i, j, item)
+        self.update_data()
 
-    def mas():
-        most_active = yf.Tickers("GME AAPL TSLA MSFT AMC")
-        stock_data = []
+    def update_data(self):
+        commodity_data = get_commodity_data()
+        num_rows, num_columns = commodity_data.shape
+        self.symboltable.setRowCount(num_rows)
+        self.symboltable.setColumnCount(num_columns)
+        self.symboltable.setHorizontalHeaderLabels(commodity_data.columns.tolist())
+        for i in range(num_rows):
+            for j in range(num_columns):
+                item = QTableWidgetItem(str(commodity_data.iat[i, j]))
+                self.symboltable.setItem(i, j, item)
 
-        for ticker_symbol in most_active.tickers:
-            ticker = yf.Ticker(ticker_symbol)
-            symbol = ticker_symbol
-            company_name = ticker.info.get('longName', '')
-            last_price = ticker.history(period="1d")['Close'][0]
-            volume = ticker.info.get('volume', '')
-            market_cap = ticker.info.get('marketCap', '')
-        
-            stock_data.append([symbol, company_name, last_price, volume, market_cap])
-
-        return stock_data
-    
-app = QtWidgets.QApplication(sys.argv)
-symbolw = WindowClass(None) 
-symbolw.show()
-app.exec_()
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    main_window = Ui_SymbolWindow()
+    main_window.show()
+    sys.exit(app.exec_())
